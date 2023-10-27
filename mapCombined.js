af7af5b9bddfffe6b832dab7230bb8a56e961718
paskr18 <146536178+paskr18@users.noreply.github.com>
2023-10-27 15:25:50 +0000

    Add files via upload

--- mapCombined.js
+++ mapCombined.js
@@ -1,6 +1,7 @@
 var map = L.map('map').setView([7.54, -5.54],6);
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
           function getColor(d) { return d>5000000?"#00441B" : d>4000000?"#006D2C" : d> 3000000?"#238B45" : d>2000000?"#41AE76" : d>1000000?"#66C2A4" : d>750000?"#99D8C9" : d>500000?"#CCECE6" : d>250000?"#E5F5F9": "#F7FCFD"; }
+          L.control.scale().addTo(map);
           function style(feature) { return {fillColor:getColor(feature.properties.population),weight:2,opacity:1,color:"black",dashArray:"3",fillOpacity:0.7};}
           function highlightFeature(e) { var layer = e.target;
                                          layer.setStyle({weight:5, color:"#666", dashArray:"", fillOpacity:0.7});
--- mapDashboard.js
+++ mapDashboard.js
@@ -1,6 +1,7 @@
 var map = L.map('map').setView([7.54, -5.54],6);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
 function getColor(d) { return d==01?"#C0362B" : d==02?"#E74C3C" : d==03?"#9B59B6" : d==04?"#8E44AD" : d==05?"#2980B9" : d==06?"#3498DB" : d==07?"#1ABC9C" : d==08?"#16A085":  d==09?"#27AE60": d==10?"#2ECC71": d==11?"#F1C40F": d==12?"#F39C12": "#E67E22" }
+L.control.scale().addTo(map);
 function style(feature) { return {fillColor:getColor(feature.id),weight:2,opacity:1,color:"black",dashArray:"3",fillOpacity:0.7};}
 function highlightFeature(e) { var layer = e.target;
                                layer.setStyle({weight:5, color:"#666", dashArray:"", fillOpacity:0.7});
