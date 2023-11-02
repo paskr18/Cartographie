d04d9b92350fb47f87352286edb19cfe891b3f05
paskr18 <146536178+paskr18@users.noreply.github.com>
2023-11-02 08:10:27 +0000

    Add files via upload

--- combinedMaps.php
+++ combinedMaps.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   </head>
 
   <body>
@@ -40,6 +41,7 @@
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
           <label for="district"> Districts : </label>
           <select id="district" name="district">
+            <option value="none" selected disabled hidden> Choisissez le district </option>
             <?php include "scripts/districtsDropdown.php";
             ?>
           </select>
@@ -48,7 +50,57 @@
           <label for="town"> Afficher les agglomérations </label> <br> <br>
           <input type="checkbox" name="health" id="health" value="health">
           <label for="town"> Afficher les installation médicales </label>
-        </form>
+        </form> 
+        <script>
+          $(document).ready(function(){
+                 $("#district").change(function(){
+                   var hDepartments = ($(this).find(":selected").val());
+                   switch (hDepartments) {
+                     case "Abidjan":
+                        $("#map").innerHTML = map.setView([5.316667, -4.033333], 10);
+                        break;
+                     case "Bas-Sassandra" :
+                        $("#map").innerHTML = map.setView([5.49998, -6.666664],8);
+                        break;
+                     case "Comoé" :
+                        $("#map").innerHTML = map.setView([6.7333, -3.4834],9);
+                        break;
+                     case "Denguélé" :
+                        $("#map").innerHTML = map.setView([9.4999, -7.417],8);
+                        break;
+                     case "Gôh-Djiboua" :
+                        $("#map").innerHTML = map.setView([6.1332, -5.9333],9);
+                        break;
+                      case "Lacs" :
+                        $("#map").innerHTML = map.setView([6.6499, -4.6999],9);
+                        break;
+                      case "Lagunes" :
+                        $("#map").innerHTML = map.setView([5.9333, -4.2167],9);
+                        break;
+                      case "Montagnes" :
+                        $("#map").innerHTML = map.setView([7.3999, -7.55],8);
+                        break;
+                      case "Sassandra-Marahoué" :
+                        $("#map").innerHTML = map.setView([6.8833, -6.45],8);
+                        break;
+                      case "Savanes" :
+                        $("#map").innerHTML = map.setView([9.4165, -5.6166],8);
+                        break;
+                      case "Vallée du Bandama" :
+                        $("#map").innerHTML = map.setView([8.1333, -5.1001],8);
+                        break;
+                      case "Woroba" :
+                        $("#map").innerHTML = map.setView([8.4832, -6.6],8);
+                        break;
+                      case "Yamoussoukro":
+                        $("#map").innerHTML = map.setView([6.8092, -5.2954],10);
+                        break;
+                      case "Zanzan" : 
+                        $("#map").innerHTML = map.setView([8.6167, -3.15],8);
+                   }
+                 });
+               });
+        </script>
         <br>
         </div>
   </body>
--- regionsDropdown.js
+++ regionsDropdown.js
@@ -5,27 +5,32 @@ $(document).ready(function(){
                      case "Abidjan":
                         $("#commune").empty();
                         $("#commune").append(new Option("Abidjan","Abidjan"));
+                        $("#map").innerHTML = map.setView([5.316667, -4.033333], 10);
                         break;
                      case "Bas-Sassandra" :
                         $("#commune").empty();
                         $("#commune").append(new Option("Gbôkié","Gbôkié"));
                         $("#commune").append(new Option("Nawa","Nawa"));
                         $("#commune").append(new Option("San-Pédro","San-Pédro"));
+                        $("#map").innerHTML = map.setView([5.49998, -6.666664],8);
                         break;
                      case "Comoé" :
                         $("#commune").empty();
                         $("#commune").append(new Option("Indénié-Djuablin","Indénié-Djuablin"));
                         $("#commune").append(new Option("Sud-Comoé","Sud-Comoé"));
+                        $("#map").innerHTML = map.setView([6.7333, -3.4834],9);
                         break;
                      case "Denguélé" :
                         $("#commune").empty();
                         $("#commune").append(new Option("Folon","Folon"));
                         $("#commune").append(new Option("Kabadougou","Kabadougou"));
+                        $("#map").innerHTML = map.setView([9.4999, -7.417],8);
                         break;
                      case "Gôh-Djiboua" :
                         $("#commune").empty();
                         $("#commune").append(new Option("Gôh","Gôh"));
                         $("#commune").append(new Option("Lôh-Djiboua","Lôh-Djiboua"));
+                        $("#map").innerHTML = map.setView([6.1332, -5.9333],9);
                         break;
                       case "Lacs" :
                         $("#commune").empty();
@@ -33,49 +38,58 @@ $(document).ready(function(){
                         $("#commune").append(new Option("Iffou","Iffou"));
                         $("#commune").append(new Option("Moronou","Moronou"));
                         $("#commune").append(new Option("NZi","NZi"));
+                        $("#map").innerHTML = map.setView([6.6499, -4.6999],9);
                         break;
                       case "Lagunes" :
                         $("#commune").empty();
                         $("#commune").append(new Option("Agnéby-Tiassa","Agnéby-Tiassa"));
                         $("#commune").append(new Option("Grands-Ponts","Grands-Ponts"));
                         $("#commune").append(new Option("Mé","Mé"));
+                        $("#map").innerHTML = map.setView([5.9333, -4.2167],9);
                         break;
                       case "Montagnes" :
                         $("#commune").empty();
                         $("#commune").append(new Option("Cavally","Cavally"));
                         $("#commune").append(new Option("Guémon","Guémon"));
                         $("#commune").append(new Option("Tonkpi","Tonkpi"));
+                        $("#map").innerHTML = map.setView([7.3999, -7.55],8);
                         break;
                       case "Sassandra-Marahoué" :
                         $("#commune").empty();
                         $("#commune").append(new Option("Haut-Sassandra","Haut-Sassandra"));
                         $("#commune").append(new Option("Marahoué","Marahoué"));
+                        $("#map").innerHTML = map.setView([6.8833, -6.45],8);
                         break;
                       case "Savanes" :
                         $("#commune").empty();
                         $("#commune").append(new Option("Bagoué","Bagoué"));
                         $("#commune").append(new Option("Poro","Poro"));
                         $("#commune").append(new Option("Tchologo","Tchologo"));
+                        $("#map").innerHTML = map.setView([9.4165, -5.6166],8);
                         break;
                       case "Vallée du Bandama" :
                         $("#commune").empty();
                         $("#commune").append(new Option("Gbêkê","Gbêkê"));
                         $("#commune").append(new Option("Hambol","Hambol"));
+                        $("#map").innerHTML = map.setView([8.1333, -5.1001],8);
                         break;
                       case "Woroba" :
                         $("#commune").empty();
                         $("#commune").append(new Option("Bafing","Bafing"));
                         $("#commune").append(new Option("Béré","Béré"));
                         $("#commune").append(new Option("Worodougou","Worodougou"));
+                        $("#map").innerHTML = map.setView([8.4832, -6.6],8);
                         break;
                       case "Yamoussoukro":
                         $("#commune").empty();
                         $("#commune").append(new Option("Yamoussoukro","Yamoussoukro"));
+                        $("#map").innerHTML = map.setView([6.8092, -5.2954],10);
                         break;
                       case "Zanzan" :
                         $("#commune").empty();
                         $("#commune").append(new Option("Bounkani","Bounkani"));
-                        $("#commune").append(new Option("Gontougou","Gontougou"));
+                        $("#commune").append(new Option("Gontougou","Gontougou")); 
+                        $("#map").innerHTML = map.setView([8.6167, -3.15],8);
                    }
                  });
                });
